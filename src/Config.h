e9a02fbcceb982a835c562ef6401e06c7dffd516
alexandre-lefebv <alexandre.lefebvre@imt-atlantique.net>
2020-12-13 19:51:58 +0100

    Added headers for behaviours compilation test

---
+++ src/Config.h
@@ -0,0 +1,32 @@
+/*
+Class Config
+
+This class is ment to read the config file and provide the config as a
+singleton.
+
+T.P
+*/
+
+#ifndef CONFIG
+#define CONFIG
+
+#include <string>
+#include <vector>
+
+
+class Config{
+  public:
+    static Config* get_instance();
+    std::string get_config_string(std::string variable_name);
+    float get_config_float(std::string variable_name);
+    int get_config_int(std::string variable_name);
+    void load_configuration(std::string filename);
+  private:
+    Config();
+    static Config* singleton;
+    void parse_str();
+    std::vector<std::string> variable_names;
+    std::vector<std::string> variable_values;
+};
+
+#endif
--- src/ICreature.h
+++ src/ICreature.h
@@ -4,39 +4,44 @@
 #include "Medium.h"
 #include "UImg.h"
 
+// Mutual inclusion
+class Medium;
+
+
 class ICreature {
     // Define operations that can be altered by decorators
 public:
     ~ICreature() = default;
+
     virtual void action(const Medium &myMedium) = 0;
 
     virtual void draw(UImg &support) = 0;
 
-    virtual const double get_camouflage() = 0;
+    virtual bool is_detected(const ICreature &c) = 0;
 
-    virtual const int get_lifetime() = 0;
+    virtual bool is_collision_deadly() = 0;
 
-    virtual const double get_speed() = 0;
+    virtual float get_speed() = 0;
 
-    virtual const int get_x() = 0;
+    virtual float get_camouflage() = 0;
 
-    virtual const int get_y() = 0;
+    virtual int get_lifetime() = 0;
 
-    virtual const double get_vx() = 0;
+    virtual float get_size() = 0;
 
-    virtual const double get_vy() = 0;
+    virtual int get_x() = 0;
 
-    virtual void init_coords(const int x, const int y) = 0;
+    virtual int get_y() = 0;
 
-    virtual const bool is_collision_deadly() = 0;
+    virtual float get_vx() = 0;
 
-    virtual const bool is_detected(const ICreature &c) = 0;
-
-    virtual void set_camouflage(const int new_val) = 0;
+    virtual float get_vy() = 0;
 
     virtual void set_lifetime(const int new_val) = 0;
 
-    virtual void set_speed(const double new_val) = 0;
+    virtual void set_coords(const int x, const int y) = 0;
+
+    virtual void set_vx_vy(const int vx, const int vy) = 0;
 };
 
 #endif //ICREATURE_H

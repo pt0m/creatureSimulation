c408990eb4fb0006f50f35363db673d1634ac43c
pt0m <tom-pegeot@hotmail.fr>
2020-12-15 16:25:45 -0500

    add creature decorator header

--- src/CreatureDecorator.h
+++ src/CreatureDecorator.h
@@ -3,11 +3,18 @@
 
 #include "ICreature.h"
 
-class CreatureDecorator {
+class CreatureDecorator : public ICreature {
  public:
   CreatureDecorator(ICreature *c);
 
   // Methods whose behavior can be modified by concrete decorators
+  void action(Medium &myMedium) override;
+  void draw(UImg &support) const override;
+  float get_camouglage() const override; 
+
+
+
+
 
   virtual const bool is_detected(const ICreature *c);
 
@@ -16,9 +23,51 @@ class CreatureDecorator {
   virtual const double get_speed();
 
   virtual const bool is_collision_deadly();
+#ifndef CREATURE_H
+#define CREATURE_H
 
- private:
+#include "IBehaviour.h"
+#include "ICreature.h"
+#include "Medium.h"
+#include "UImg.h"
+
+class Creature : public ICreature {
+  
+ public:
+
+  void action(Medium &myMedium) override;
+
+  void draw(UImg &support) const override;
+
+  float get_camouflage() const override;
+
+  int get_lifetime() const override;
+
+  float get_size() const override;
+
+  float get_speed() const override;
+
+  int get_x() const override;
+
+  int get_y() const override;
+
+  float get_vx() const override;
+
+  float get_vy() const override;
+
+  bool is_collision_deadly() const override;
+
+  bool is_detected(const ICreature &c) const override;
+
+  void set_coords(const int x, const int y) override;
+
+  void set_lifetime(const int new_val) override;
+
+  void set_vx_vy(const int vx, const int vy) override;
+
+  ~CreatureDecorator(); // il faut penser a appeler le destructeur de decoree
+private:
+  
   ICreature *decoree;
 };
-
 #endif //CREATUREDECORATOR_H

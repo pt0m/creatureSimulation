232f94465ff4eb5d21cf66f84a41241321f1573f
alexandre-lefebv <alexandre.lefebvre@imt-atlantique.net>
2020-12-15 20:34:25 +0100

    Ordered methods prototype

--- src/ICreature.h
+++ src/ICreature.h
@@ -16,17 +16,13 @@ class ICreature {
 
   virtual void draw(UImg &support) const = 0;
 
-  virtual bool is_detected(const ICreature &c) const = 0;
-
-  virtual bool is_collision_deadly() const = 0;
-
-  virtual float get_speed() const = 0;
-
   virtual float get_camouflage() const = 0;
 
   virtual int get_lifetime() const = 0;
 
   virtual float get_size() const = 0;
+
+  virtual float get_speed() const = 0;
 
   virtual int get_x() const = 0;
 
@@ -36,9 +32,13 @@ class ICreature {
 
   virtual float get_vy() const = 0;
 
-  virtual void set_lifetime(const int lifetime) = 0;
+  virtual bool is_collision_deadly() const = 0;
+
+  virtual bool is_detected(const ICreature &c) const = 0;
 
   virtual void set_coords(const int x, const int y) = 0;
+
+  virtual void set_lifetime(const int lifetime) = 0;
 
   virtual void set_vx_vy(const int vx, const int vy) = 0;
 };

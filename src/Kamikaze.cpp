ddf63b26c1196733fa8a5852ae98b777a1bb1b2d
alexandre-lefebv <alexandre.lefebvre@imt-atlantique.net>
2020-12-17 05:26:53 +0100

    Debuged kamize find closest neighbour

--- src/Kamikaze.cpp
+++ src/Kamikaze.cpp
@@ -26,7 +26,7 @@ void Kamikaze::next_step(ICreature* creature, Medium* my_medium){
         int max_dy = my_medium->get_height();
 
         double curr_sq_dist, min_sq_dist = max_dx*max_dx+max_dy*max_dy;
-        int target_x, target_y, curr_x,curr_y;
+        int target_x, target_y, curr_x, curr_y, rel_x, rel_y ;
 
         // Go trough the neighbours list to find the closest
         std::list<ICreature*>::iterator iter;
@@ -35,7 +35,9 @@ void Kamikaze::next_step(ICreature* creature, Medium* my_medium){
             it = *iter;
             curr_x = it->get_x();
             curr_y = it->get_y();
-            curr_sq_dist = curr_x*curr_x+curr_y*curr_y;
+            rel_x = curr_x-new_x;
+            rel_y = curr_y-new_y;
+            curr_sq_dist = rel_x*rel_x+rel_y*rel_y;
 
             if (curr_sq_dist<min_sq_dist){
                 target_x = curr_x;
@@ -47,15 +49,20 @@ void Kamikaze::next_step(ICreature* creature, Medium* my_medium){
         double dir_x = target_x-new_x;
         double dir_y = target_y-new_y;
         double dir_norm = sqrt(dir_x*dir_x+dir_y*dir_y);
+        if (dir_norm>0.01){
+            double speed_norm = creature->get_speed();
 
-        double speed_norm = creature->get_speed();
+            double new_vx = dir_x*speed_norm/dir_norm;
+            double new_vy = dir_y*speed_norm/dir_norm;
+            creature->set_vx_vy(new_vx,new_vy);
 
-        double new_vx = dir_x/dir_norm*speed_norm;
-        double new_vy = dir_y/dir_norm*speed_norm;
-        creature->set_vx_vy(new_vx,new_vy);
-
-        new_x += new_vx;
-        new_y += new_vy;
+            new_x += new_vx;
+            new_y += new_vy;
+        }
+        else{
+            new_x += creature->get_vx();
+            new_y += creature->get_vy();
+        }
     }
     // Continue toward the same direction if there is no neighours
     else{
